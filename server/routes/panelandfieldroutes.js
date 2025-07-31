75c9d948808ef38d6d65f656e3bfb49cacc9c65d
Ram Mishra <ram1994mishra@gmail.com>
2025-07-31 18:55:34 +0530

    evening commit

--- server/routes/panelandfieldroutes.js
+++ server/routes/panelandfieldroutes.js
@@ -8,6 +8,7 @@ router.get('/panel', Panelcontroller.find);
 router.get('/panel/:id', Panelcontroller.find);
 router.post('/addPanel', Panelcontroller.addPanelType);
 router.post('/addMultiplePanel', Panelcontroller.addMultiplePanels);
+router.put('/updatePanelOrder', Panelcontroller.updatePanelOrder);
 router.put('/updatePanel/:id', Panelcontroller.updatePanelType);
 router.delete('/deletePanel/:id', Panelcontroller.deletePanelType);
 
@@ -16,6 +17,7 @@ router.get('/fields', Fieldtypecontroller.findField);
 router.get('/field/:id', Fieldtypecontroller.findField);
 router.post('/addFields', Fieldtypecontroller.addFieldType);
 router.post('/addMultipleFieldType', Fieldtypecontroller.addMultipleFieldTypes);
+router.put('/updateFieldOrder', Fieldtypecontroller.updateFieldOrder);
 router.put('/updateField/:id', Fieldtypecontroller.updateFieldType);
 router.delete('/deleteField/:id', Fieldtypecontroller.deleteFieldType);
 
--- webapp/src/app/components/fielddetails/fielddetails.ts
+++ webapp/src/app/components/fielddetails/fielddetails.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Fieldinterface } from '../../interfaces/fieldinterface';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { Fieldservice } from '../../services/fieldservice';
 
 @Component({
   selector: 'app-fielddetails',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, DragDropModule],
   templateUrl: './fielddetails.html',
   styleUrl: './fielddetails.scss'
 })
@@ -14,22 +17,59 @@ export class Fielddetails {
   leftFields: Fieldinterface[] = [];
   rightFields: Fieldinterface[] = [];
 
-  constructor() { }
+  constructor(private fieldService: Fieldservice) { }
 
   ngOnChanges(): void {
     if (!this.fields) return;
-
+    const fieldsCopy = [...this.fields];
     // Reset first
-    this.leftFields = this.fields
+    this.leftFields = fieldsCopy
       .filter(f => f.colId === 0)
       .sort((a, b) => a.orderId - b.orderId);
 
-    this.rightFields = this.fields
+    this.rightFields = fieldsCopy
       .filter(f => f.colId === 1)
       .sort((a, b) => a.orderId - b.orderId);
 
     console.log('Left Fields:', this.leftFields);
     console.log('Right Fields:', this.rightFields);
+  }
+
+  dropField(event: CdkDragDrop<Fieldinterface[]>) {
+    
+    if (event.previousContainer === event.container) {
+      // Moved within the same list
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      // Moved between left and right columns
+      console.log("Moved between left and right columns");
+      
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+
+      // Update colId when moved across
+      const movedItem = event.container.data[event.currentIndex];
+      movedItem.colId = event.container.id === 'leftColumn' ? 0 : 1;
+
+    }
+
+    // Update orderId after reordering
+    this.leftFields.forEach((field, index) => field.orderId = index + 1);
+    this.rightFields.forEach((field, index) => field.orderId = index + 1);
+
+    console.log("Updated left:", this.leftFields);
+    console.log("Updated right:", this.rightFields);
+
+    const updatedFields = [...this.leftFields, ...this.rightFields];
+    this.fieldService.updateFieldOrder(updatedFields).subscribe({
+      next: (res) => console.log("Fields reordered successfully.", res),
+      error: (err) => console.error("Update failed", err)
+    });
+
   }
 
 
